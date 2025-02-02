44de9740d4c55ebc2f15ec3631c778085405f709
jjcmjavascript <jjcmjavascript@gmail.com>
2025-02-02 18:52:21 -0300

    feat: agregar banner de Google en el componente TarotSlider y mejorar diseño de presentación

--- app/modules/tarot/components/tarot-slider.component.tsx
+++ app/modules/tarot/components/tarot-slider.component.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Dimensions, Image, Text } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useTarotStore } from '../tarot.store';
+import { GoogleBanner } from '@/shared/components/google-banner.component';
 
 const { width, height } = Dimensions.get('screen');
 const cardWidth = width / 4;
@@ -13,13 +14,14 @@ export function TarotSlider() {
 
   return (
     <View style={styles.container}>
+      <GoogleBanner />
       <Carousel
         loop={true}
         width={width * 0.9}
         autoPlay={false}
         data={data}
         scrollAnimationDuration={800}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           <ScrollView style={styles.scrollView}>
             <View style={styles.cardContainer}>
               {item.cardUrls.map((card, index) => (
@@ -34,9 +36,21 @@ export function TarotSlider() {
                   key={index}
                 />
               ))}
-            </View>
+              <Text style={styles.text}>{item.reading}</Text>
 
-            <Text style={styles.text}>{item.reading}</Text>
+              <Text
+                style={{
+                  position: 'absolute',
+                  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                  padding: 5,
+                  borderRadius: 10,
+                  top: -20,
+                  right: -20,
+                }}
+              >
+                {index + 1} de {data.length}
+              </Text>
+            </View>
           </ScrollView>
         )}
       />
@@ -48,10 +62,11 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
-    height: height * 0.8,
+    height: height * 0.75,
     zIndex: 1,
   },
   scrollView: {
+    marginTop: 20,
     borderRadius: 20,
     backgroundColor: 'rgba(255, 255, 255, 0.92)',
     padding: 20,
@@ -62,6 +77,7 @@ const styles = StyleSheet.create({
     gap: 4,
     width: '100%',
     justifyContent: 'center',
+    height: height * 0.75,
   },
   cardImage: {
     width: '100%',
